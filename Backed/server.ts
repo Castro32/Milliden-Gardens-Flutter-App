1721e6a8c61acf69c2de82b1160a43ce9726b78d
castro32 <okanga.fidel@gmail.com>
2025-09-06 16:16:37 +0300

    refactor: update CORS configuration to allow additional localhost origin

--- Backed/server.ts
+++ Backed/server.ts
@@ -251,6 +251,7 @@ app.use(cors({
       'http://127.0.0.1:3000',
       'http://127.0.0.1:8080',
       'http://127.0.0.1:43013',
+      'http://localhost:38710',
       'https://milliden-gardens.vercel.app'
     ];
     
